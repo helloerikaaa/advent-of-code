576e6d444635b69520013bd1f42d2724c57fadfe
Erika <esanchezf1201@alumno.ipn.mx>
2021-12-08 12:33:52 -0600

    Part 2 of day 2

---
+++ day02/part_2.py
@@ -0,0 +1,26 @@
+def main():
+    commands = []
+    with open('input.txt') as f:
+        for line in f.readlines():
+            key, value = line.split(' ')
+            commands.append((key, int(value)))
+
+    position = 0
+    aim = 0
+    depth = 0
+    for i in range(0, len(commands)):
+        if commands[i][0] == 'forward':
+            position += commands[i][1]
+            depth += (aim) * commands[i][1]
+        elif commands[i][0] == 'up':
+            aim -= commands[i][1]
+        elif commands[i][0] == 'down':
+            aim += commands[i][1]
+
+    final_config = depth * position
+
+    print(f'Final position and depth is {final_config}')
+
+
+if __name__ == '__main__':
+    main()
