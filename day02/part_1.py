704d4001b0a156316209bfc83d54ff284a93d925
Erika <esanchezf1201@alumno.ipn.mx>
2021-12-08 12:25:23 -0600

    Optimal day02 part one solution

--- day02/part_1.py
+++ day02/part_1.py
@@ -3,19 +3,22 @@ def main():
     with open('input.txt') as f:
         for line in f.readlines():
             key, value = line.split(' ')
-            commands.append({key: int(value)})
+            commands.append((key, int(value)))
 
-    position = sum(command['forward']
-                   for command in commands if command.get('forward'))
-    depth_down = sum(command['down']
-                     for command in commands if command.get('down'))
-    depth_up = sum(command['up'] for command in commands if command.get('up'))
+    position = 0
+    depth = 0
+    
+    for i in range(0, len(commands)):
+        if commands[i][0] == 'forward':
+            position += commands[i][1]
+        elif commands[i][0] == 'up':
+            depth -= commands[i][1]
+        elif commands[i][0] == 'down':
+            depth += commands[i][1]
+    
+    final_config = depth * position
 
-    final_depth = depth_down - depth_up
-
-    final = position * final_depth
-
-    print(f'Final position and depth is {final}')
+    print(f'Final position and depth is {final_config}')
 
 
 if __name__ == '__main__':
